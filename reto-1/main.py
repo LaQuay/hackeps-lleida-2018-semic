38c9e435f8bdc89c10f32444a89ef67a8137f71c
elorenteg <ester.lorente@est.fib.upc.edu>
2018-11-18 03:54:56 +0100

    add reto 1

--- reto-1/main.py
+++ reto-1/main.py
@@ -1,22 +1,21 @@
-
 def numsecs(N):
     ns = 0
 
-    other_dig = N[0:len(N)-1]
-    last_dig = N[len(N)-1]
+    other_dig = N[0:len(N) - 1]
+    last_dig = N[len(N) - 1]
 
-    while (int(other_dig) - 2*int(last_dig) >= 0):
+    while (int(other_dig) - 2 * int(last_dig) >= 0):
         ns += 1
-        N = str(int(other_dig) - 2*int(last_dig))
-        #if "7" in N: ns += 1
+        N = str(int(other_dig) - 2 * int(last_dig))
+        # if "7" in N: ns += 1
         count7 = N.count("7")
         ns += count7
 
-        #print(other_dig + "-" + str(2*int(last_dig)) + "=" + N)
+        # print(other_dig + "-" + str(2*int(last_dig)) + "=" + N)
 
         if len(N) == 1: break
-        other_dig = N[0:len(N)-1]
-        last_dig = N[len(N)-1]
+        other_dig = N[0:len(N) - 1]
+        last_dig = N[len(N) - 1]
 
     ns += 1
 
@@ -31,5 +30,4 @@ if __name__ == "__main__":
         N = f.readline().strip()
 
         nsec = numsecs(N)
-        print ("Case #" + str(i+1) + ": " + str(nsec))
-
+        print("Case #" + str(i + 1) + ": " + str(nsec))
