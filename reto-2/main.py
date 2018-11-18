67c4270c5c175a4c65211a93f48fdebba550b8b0
elorenteg <ester.lorente@est.fib.upc.edu>
2018-11-18 03:54:40 +0100

    add reto 2

--- reto-2/main.py
+++ reto-2/main.py
@@ -1,57 +1,57 @@
-
 import sys
+
 sys.setrecursionlimit(15000)
 
 dicc_div = {}
+
 
 def numdivisions(N):
     nd = 0
 
     i = 1
     while (i <= N):
-        ndi = numdivisionsbynum(N,i)
+        ndi = numdivisionsbynum(N, i)
         nd += ndi
-        #print (str(i) + "--" + str(ndi))
-        i +=1
+        # print (str(i) + "--" + str(ndi))
+        i += 1
 
     return nd
+
 
 def numdivisionsbynum(N, num):
     if num == 1:
         return 1
     elif num == 2:
-        return N//num
+        return N // num
     else:
-        nd = N//num
+        nd = N // num
         while (N >= num):
             N = N - num
             i = 2
             while (i < num):
-                if (N,i) in dicc_div:
-                    ndi = dicc_div[(N,i)]
+                if (N, i) in dicc_div:
+                    ndi = dicc_div[(N, i)]
                 else:
                     ndi = numdivisionsbynum(N, i)
-                    dicc_div[(N,i)] = ndi
-                #print (" > " + str(i) + "--" + str(ndi))
+                    dicc_div[(N, i)] = ndi
+                # print (" > " + str(i) + "--" + str(ndi))
                 i += 1
                 nd += ndi
         return nd
 
-def stack(lim, ways):
-    posi = [0] * (lim + 1)
-    posi[0] = 1
 
-    for i in ways:
-        #print(i)
-        for k in range(i, lim + 1):
-            posi[k] += posi[k - i]
+import wolframalpha
 
-    return posi[lim]
 
+def wolfram_partitions(N):
+    client = wolframalpha.Client("53L6R8-YYEQG7A92E")
+    res = client.query("partitionP(" + str(N) + ")")
+    return next(res.results).text
 
 
 if __name__ == "__main__":
-    f = open('input2.txt')
+    f = open('input.txt')
     N = int(f.readline().strip())
     np = numdivisions(N)
+    #np = wolfram_partitions(N)
     print("Case #" + str(N) + ": " + str(np))
